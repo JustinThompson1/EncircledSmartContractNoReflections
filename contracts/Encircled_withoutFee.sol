6e0f656b3b7469661654b8e7c1637bd49283a16b
Lukas Ehlers <ehlers@outson.com>
2023-01-11 11:06:16 +0100

    adding ownable

--- contracts/Encircled_withoutFee.sol
+++ contracts/Encircled_withoutFee.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Encircled is ERC20 {
+contract Encircled is ERC20, Ownable {
     constructor() ERC20("Encircled", "ENCD") {
         _mint(msg.sender, 200000000 * 10 ** decimals());
     }
